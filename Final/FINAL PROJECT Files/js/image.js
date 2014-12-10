35c66bda9d4125701ab3933d26ce745fe5e98450
wernerry <wernerry@msu.edu>
2014-12-10 00:54:17 -0500

    Final Project

---
+++ Final/FINAL PROJECT Files/js/image.js
@@ -0,0 +1,71 @@
+function OnPressImage1()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "img/1.jpg";
+}
+
+function OnPressImage2()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "img/2.jpg";
+}
+
+function OnPressImage3()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "img/3.jpg";
+}
+
+function OnPressImage4()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "img/4.jpg";
+}
+
+function OnPressImage5()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "img/5.jpg";
+}
+
+function OnPressImage6()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "img/6.jpg";
+}
+
+function OnPressImage7()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "img/7.jpg";
+}
+
+function OnPressImage8()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "img/8.jpg";
+}
+
+function OnPressImage9()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "img/9.jpg";
+}
+
+function OnPressImage10()
+{
+	var imageElement = document.getElementById("bigPicture");
+	
+	imageElement.src = "img/10.jpg";
+}
+
+window.onload = init;
